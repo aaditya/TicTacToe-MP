77432aa3c44d3dd7aeb48436bec8e668ef5b8aed
Aaditya Chakravarty <aaditya.chakravarty@shippigo.com>
2019-10-25 18:48:36 +0530

    Deleted

--- public/script.js
+++ public/script.js
@@ -43,28 +43,64 @@ const reset = () => {
   }
 }
 
+// const mapTrack = (x, y) => {
+//   for (let i = 0; i < dimensions; i++) {
+//     // For X-axis
+//     let p1xm = track.filter(t => t.player == 1 && t.position.x == i + 1).length;
+//     let p2xm = track.filter(t => t.player == 2 && t.position.x == i + 1).length;
+
+//     // For Y-axis
+//     let p1ym = track.filter(t => t.player == 1 && t.position.y == i + 1).length;
+//     let p2ym = track.filter(t => t.player == 2 && t.position.y == i + 1).length;
+
+//     // Diagonal Cases
+//     let p1dm = track.filter(t => t.player == 1 && t.position.x == t.position.y).length;
+//     let p2dm = track.filter(t => t.player == 2 && t.position.x == t.position.y).length;
+
+//     if (p1xm == 3 || p1ym == 3 || p1dm == 3) {
+//       victory(1);
+//     }
+//     if (p2xm == 3 || p2ym == 3 || p2dm == 3) {
+//       victory(2);
+//     }
+//   }
+
+// }
+
 const mapTrack = () => {
+
   const p1m = track.filter(move => move.player == 1);
   const p2m = track.filter(move => move.player == 2);
 
   // horizontal and diagonal check;
   const p1ym = p1m.map(move => move.position.y).sort();
   const p2ym = p2m.map(move => move.position.y).sort();
+  if (p1ym.length == 3 && checkHorDiaCondition(p1ym)) {
+    victory(1);
+  }
+  if (p2ym.length == 3 && checkHorDiaCondition(p2ym)) {
+    victory(2);
+  }
 
   // vertical condition
   const p1xm = p1m.map(move => move.position.x).sort();
   const p2xm = p2m.map(move => move.position.x).sort();
 
-  if ((p1ym.length == 3 && checkSuccesstion(p1ym) || (p1xm.length == 3 && checkSuccesstion(p1xm)))) {
+  if (p1xm.length == 3 && checkHorDiaCondition(p1xm)) {
     victory(1);
   }
-  if ((p2ym.length == 3 && checkSuccesstion(p2ym)) || (p2xm.length == 3 && checkSuccesstion(p2xm))) {
+  if (p2ym.length == 3 && checkHorDiaCondition(p2ym)) {
     victory(2);
   }
+
 }
 
 
-const checkSuccesstion = ([first, second, third]) => first == 1 && second == 2 && third == 3
+const checkHorDiaCondition = (arr) => {
+  const [first, second, third] = arr;
+  return first == 1 && second == 2 && third == 3
+}
+
 
 
 const generateBoard = (dim) => {
